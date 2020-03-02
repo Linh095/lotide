ea961d9e8455ac6a5401c9bc40fbe770dc876b75
Linh095 <linh.nguyen.nvhl@gmail.com>
2020-03-02 23:12:32 +0000

    made file with head function returning first element of an array

---
+++ head.js
@@ -0,0 +1,21 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`✔️✔️✔️ Assertion Passed: ${actual} === ${expected}`);
+  } else  {
+    return console.log(`❌️❌️❌️❌️ ❌️  Assertion Failed: d: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+//assertEqual(1, 1);
+
+//HEAD FUNCTION
+const head = function(array) {
+  return array[0];
+};
+
+//test code
+//console.log(head([1, 2, 3]));
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
