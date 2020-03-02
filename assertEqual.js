5b66174703dc2220f02e73e9af627b31be3f7b6c
Linh095 <linh.nguyen.nvhl@gmail.com>
2020-03-02 22:03:36 +0000

    es6 template used

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    return console.log("✔️✔️✔️ Assertion Passed: " + actual + " === " + expected);
+    return console.log(`✔️✔️✔️ Assertion Passed: ${actual} === ${expected}`);
   } else  {
-    return console.log("❌️❌️❌️ Assertion Failed: " + actual + " !== " + expected);
+    return console.log(`❌️❌️❌️❌️ ❌️  Assertion Failed: d: ${actual} !== ${expected}`);
   }
 };
 
