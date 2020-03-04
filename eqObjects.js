230e4b910af5fdac8b0d4ed5bde2476eb277c9ba
Linh095 <linh.nguyen.nvhl@gmail.com>
2020-03-04 21:12:06 +0000

    made eqObject.js able to compare values that are arrays

--- eqObjects.js
+++ eqObjects.js
@@ -6,6 +6,19 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let x = 0; x < array1.length; x++) {
+      if (array1[x] !== array2[x]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
 const eqObjects = function(object1, object2) {
   const keysObject1 = Object.keys(object1);
   const keysObject2 = Object.keys(object2);
@@ -13,19 +26,32 @@ const eqObjects = function(object1, object2) {
     return false;
   }
   for (const key in object1) {
-    if (object1[key] !== object2[key]) {
-      return false;
+    const value1 = object1[key];
+    const value2 = object2[key];
+
+    if (Array.isArray(value1)) {
+      if (!eqArrays(value1,value2)) {
+        return false;
+      }
+    } else {
+      if (object1[key] !== object2[key]) {
+        return false;
+      }
     }
   }
   return true;
 };
 
 //TEST CODE
-/*
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
+//const ab = { a: "1", b: "2" };
+//const ba = { b: "2", a: "1" };
+
+const ab = { a: [1,2], b: [2,2] };
+const ba = { b: [2,2], a: [1,2] };
+
 console.log(assertEqual(eqObjects(ab, ba), true));
 
-const abc = { a: "1", b: "2", c: "3" };
+// const abc = { a: "1", b: "2", c: "3" };
+const abc = { a: "1", b: "2"};
+
 console.log(assertEqual(eqObjects(ab, abc),false));
-*/
